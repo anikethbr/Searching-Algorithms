5efad378633798b0aaf47280a98ba14460ee1e15
Aniketh B R <88420285+anikethbr@users.noreply.github.com>
2021-10-07 15:36:45 +0530

    Update LinearSearch.c

--- LinearSearch.c
+++ LinearSearch.c
@@ -6,14 +6,18 @@ int main()
     printf("Enter Number of elements:\n");
     scanf("%d",&n);
     int a[n];
-    printf("Enter elements:\n");
+    printf("Enter array elements:\n");
     for(int i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
     printf("Enter element to be searched:\n");
     scanf("%d",&ele);
-    int b=linear_search(a,ele,n);
-    printf("%d",b);
+    int result=linear_search(a,ele,n);
+    if(result==-1){
+        printf("Element not found\n");
+    }else{
+        printf("Element found at position :%d\n",result);
+    }
     return 0;
 }
 int linear_search(int arr[],int key,int size){
